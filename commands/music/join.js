de79eb9d6dad7a9fc1916cdc2565f9b6449feb39
Sovan Mondal <56794254+sovanmondal182@users.noreply.github.com>
2022-09-11 23:36:51 +0530

    Update join.js

--- commands/music/join.js
+++ commands/music/join.js
@@ -5,13 +5,13 @@ module.exports = {
     utilisation: '{prefix}join',
 
     execute(client, message) {
-        const channel = client.channels.cache.get("968450564318978091");
+        const channel = client.channels.cache.get("VOICE CHANNEL ID");
         if (!channel) return console.error("The channel does not exist!");
         channel.join().then(connection => {
             return message.channel.send({
                 embed: {
                     color: 'RANDOM',
-                    description: `${client.emotes.success} - Successfully Joined <#968450564318978091> !`},
+                    description: `${client.emotes.success} - Successfully Joined <#VOICE CHANNEL ID> !`},
                 });
         }).catch(e => {
             console.error(e);
